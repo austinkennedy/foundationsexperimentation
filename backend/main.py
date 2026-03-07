040849fdf05ad7f8a4373b00da0d5a513dc7c1fe
Austin Kennedy <akenns1947@gmail.com>
2026-03-07 12:36:38 -0700

    Fixed results card placement

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
         "https://foundationsexperimentation.com",
